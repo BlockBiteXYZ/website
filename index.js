97d0964f0fae78cbf8d5842d9311a29d0d1b0694
Mathias Scherer <scherer.mat@gmail.com>
2022-12-29 15:31:09 +0100

    small change to the title

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const title = document.getElementById("title");
 
 const typewriterTitle = new Typewriter(title, {
   loop: false,
-  delay: 100,
+  delay: 250,
   cursor: "▍",
 });
 
@@ -21,7 +21,7 @@ const typewriterContactUs = new Typewriter(contactUs, {
   cursor: "",
 });
 
-typewriterTitle.pauseFor(250).typeString("BlockBite").callFunction(() => {
+typewriterTitle.pauseFor(500).typeString("BB").pauseFor(1500).deleteChars(1).typeString("lockBite").callFunction(() => {
   typewriterSubtitle.pauseFor(250).typeString("We do blockchain stuff").callFunction(() => {
     typewriterContactUs.pauseFor(250).typeString('<a href="mailto:contact@blockbite.xyz">Contact us</a>').start()
   }).start();
